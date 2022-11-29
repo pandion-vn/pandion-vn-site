3e3ff5a1800a5683298b802d4cdd401c3286cc70
Huy-Thai <huythai.dev@gmail.com>
2022-11-29 09:45:04 +0700

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,27 @@ import '../styles/css/materialdesignicons.min.css';
 import '../styles/css/tiny-slider.css';
 import '../styles/css/style.css';
 import '../styles/css/colors/default.css';
+import Script from 'next/script';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-LN35WDRJPT"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LN35WDRJPT');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
